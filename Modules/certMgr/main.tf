97b4288ec54e0caee6e14426a1531e804503af54
AmitGujar <amithero3342@gmail.com>
2024-03-15 11:57:25 +0530

    issuer added

--- Modules/certMgr/main.tf
+++ Modules/certMgr/main.tf
@@ -9,3 +9,41 @@ resource "helm_release" "cert-manager" {
     value = "true"
   }
 }
+
+
+# resource "time_sleep" "wait" {
+#   depends_on      = [helm_release.cert-manager]
+#   create_duration = "20s"
+# }
+
+resource "kubernetes_manifest" "issuer" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "ClusterIssuer"
+    metadata = {
+      "name" = "letsencrypt-production"
+    }
+
+    spec = {
+      acme = {
+        "server" = "https://acme-v02.api.letsencrypt.org/directory"
+        "email"  = "amitgujar3342@gmail.com"
+        privateKeySecretRef = {
+          name = "letsencrypt-production"
+        }
+        solvers = [
+          {
+            http01 = {
+              ingress = {
+                "class" = "nginx"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+  depends_on = [
+    helm_release.cert-manager
+  ]
+}
