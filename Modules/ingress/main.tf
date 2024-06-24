d0b5a237133ad56ef9cdec101394fe134e7a547c
AmitGujar <amithero3342@gmail.com>
2024-06-24 10:59:22 +0530

    internal load balancer added

--- Modules/ingress/main.tf
+++ Modules/ingress/main.tf
@@ -11,9 +11,19 @@ resource "helm_release" "public-ingress" {
     name  = "controller.service.type"
     value = "LoadBalancer"
   }
+  # set {
+  #   name  = "controller.service.externalTrafficPolicy"
+  #   value = "Local"
+  # }
+
   set {
-    name  = "controller.service.externalTrafficPolicy"
-    value = "Local"
+    name  = "controller.service.internal.enabled"
+    value = true
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-internal"
+    value = "true"
   }
 
   # values = [file("${path.module}/values.yaml")] #! uncomment this only if you are using azure service 
